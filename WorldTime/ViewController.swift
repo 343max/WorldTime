917a55cacfb412ea84842d6bfd887d9f63bd2624
Max von Webel <max@343max.de>
2014-09-21 14:38:28 +0200

    Initial Commit

---
+++ WorldTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTime
+//
+//  Created by Max von Webel on 21/09/14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
